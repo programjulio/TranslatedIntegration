cf1ef4e8308b6c1cb867f6dad051a40f40333481
programjulio <programjulio@gmail.com>
2023-04-17 20:00:46 -0300

    commit do projeto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Google.Cloud.Translation.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradutorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = TranslationClient.CreateFromApiKey(Utility.apiKey);
+            var response = client.TranslateText("קוד הדגמה", LanguageCodes.Hebrew, LanguageCodes.Portuguese);
+            Console.WriteLine(response.TranslatedText);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradutorApp
+{
+    public class Utility
+    {
+        public const string apiKey = "api key";
+    }
+}
